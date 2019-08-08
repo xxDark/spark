14f7621c125e64a9d473ab165eacd12d12a3c1bb
xDark <me@xdark.dev>
2019-08-08 21:39:09 +0300

    Add https://github.com/perwendel/spark/pull/936

--- src/main/java/spark/http/matching/Routes.java
+++ src/main/java/spark/http/matching/Routes.java
@@ -56,6 +56,7 @@ final class Routes {
                     context.requestWrapper().changeMatch(match);
                 }
 
+                context.setMatched(true);
                 context.responseWrapper().setDelegate(context.response());
 
                 Object element = route.handle(context.requestWrapper(), context.responseWrapper());
